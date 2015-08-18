dfd6d0ff355c12cfea478fa89757ac5c5629070d
Peter Muturi <pmuturi65@gmail.com>
2015-08-18 08:27:33 +0300

    ranges max min..

---
+++ range.rb
@@ -0,0 +1,15 @@
+digits = 0..9
+
+puts digits.include?(5)
+ret = digits.min
+puts "Min value is #{ret}"
+
+ret = digits.max
+puts "Max value is #{ret}"
+
+ret = digits.reject {|i| i < 5 }
+puts "Rejected values are #{ret}"
+
+digits.each do |digit|
+   puts "In Loop #{digit}"
+end
